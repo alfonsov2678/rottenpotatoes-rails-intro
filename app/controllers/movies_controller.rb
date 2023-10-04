b5c379817f1f8522bc74c916bd53599fb320d400
alv2145 Alfonso Velasco <alv2145@columbia.edu>
2023-10-04 21:17:17 +0000

    testing restfulness

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -97,14 +97,17 @@ class MoviesController < ApplicationController
       if params[:sortColumn].blank?
         if !(session[:ratings].blank? && session[:sort_column].blank?):
           redirect_to movies_path(sortColumn: '', ratings: Hash[Movie.all_ratings.collect { |item| [item, '1'] }])
+        end
       else
         if !(session[:ratings].blank?):
           redirect_to movies_path(sortColumn: params[:sort_column], ratings: Hash[Movie.all_ratings.collect { |item| [item, '1'] }])
+        end
       end
     else
       if params[:sortColumn].blank?
         if !(session[:sort_column].blank?):
           redirect_to movies_path(sortColumn: '', ratings: params[:ratings])
+        end
       end
     end
     
