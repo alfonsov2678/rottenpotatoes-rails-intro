0d4a0432033bb2acd404d46b2fd2a6e58293c370
alv2145 Alfonso Velasco <alv2145@columbia.edu>
2023-10-04 03:51:10 +0000

    Updating code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
-class Movie < ApplicationRecord
+class Movie < ActiveRecord::Base
   def self.with_ratings(ratings_list)
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
