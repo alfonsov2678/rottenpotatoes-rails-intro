aae41b6209f6361a388588df7c794a36d8ffb143
alv2145 Alfonso Velasco <alv2145@columbia.edu>
2023-10-04 15:18:56 +0000

    testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,5 +12,6 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     ratings_hashmap = ['G', 'PG', 'PG-13', 'R'].each_with_object({}) do |item, hash|
       hash[item] = 1
+    end
   end
  end
